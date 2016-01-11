4d2df9acf24635992068c0de829e595d422fb39f
nicolasblaise <nicolas.blaise@isen-lille.fr>
2016-01-11 11:46:53 +0100

    AccueilServlet

---
+++ src/main/resources/isen/jee/mastermind/AccueilServlet.java
@@ -0,0 +1,12 @@
+package isen.jee.mastermind;
+
+public class AccueilServlet extends HttpServlet  {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+}
