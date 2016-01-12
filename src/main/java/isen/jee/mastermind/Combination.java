81a9c04adc0767b6963e4046e2e569e927982619
damederes <jean.delbecque@isen-lille.fr>
2016-01-12 11:22:02 +0100

    correction get

--- src/main/java/isen/jee/mastermind/Combination.java
+++ src/main/java/isen/jee/mastermind/Combination.java
@@ -18,12 +18,12 @@ public class Combination {
 		for (int i = 0; i < 4; i++){
 			for (int j = 0; j < 4; j++){
 				if(i == j){
-					if(combination[i].color == combinationArray[j].color){
+					if(combination[i].getColor() == combinationArray[j].getColor()){
 						goodColorGoodPlace+=1;
 					}		
 				}
 				else{
-					if(combination[i].color == combinationArray[j].color){
+					if(combination[i].getColor() == combinationArray[j].getColor()){
 						goodColorBadPlace+=1;
 					}
 				}
--- src/test/java/isen/jee/mastermind/CombinationTest.java
+++ src/test/java/isen/jee/mastermind/CombinationTest.java
@@ -1,5 +1,7 @@
 package isen.jee.mastermind;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class CombinationTest {
@@ -9,12 +11,30 @@ public class CombinationTest {
 		Combination cb = new Combination();
 		cb.createCombination();
 		
-		for (int i = 0; i < cb.combinationArray.length)
+		for (int i = 0; i < cb.combinationArray.length; i++){
+			
+		}
 	}
 	
 	@Test
 	public void it_can_compare_combinations() throws Exception{
 		
 	}
-
+	
+	@Test
+	public void it_can_generate_random_combination()throws Exception {
+		Combination combination1 = new Combination();
+		Combination combination2 = new Combination();
+		
+		combination1.createCombination();
+		combination2.createCombination();
+		
+		boolean isEquals = true;
+			for (int i = 0; i<4; i++){
+				if (combination1.combinationArray[i] != combination2.combinationArray[i]) {
+					isEquals = false;
+				}
+			}
+			assertFalse(isEquals);
+	}
 }
