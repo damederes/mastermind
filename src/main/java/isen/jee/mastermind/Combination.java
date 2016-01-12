3ef49c164ed304b4249eb70ab0f253960173dd36
nicolasblaise <nicolas.blaise@isen-lille.fr>
2016-01-12 10:00:04 +0100

    Classe combination

--- src/main/java/isen/jee/mastermind/Combination.java
+++ src/main/java/isen/jee/mastermind/Combination.java
@@ -2,4 +2,36 @@ package isen.jee.mastermind;
 
 public class Combination {
 
+	Piece combinationArray[] = new Piece[4];
+	int responseArray[] = new int[2];
+
+	public void createCombination() {
+		for (int i = 0; i < 4; i++) {
+			combinationArray[i] = new Piece();
+			combinationArray[i].chooseRandomColor();
+		}
+	}
+
+	public void testCombination(Piece combination[]) {
+		int goodColorGoodPlace = 0;
+		int goodColorBadPlace = 0;
+		for (int i = 0; i < 4; i++){
+			for (int j = 0; j < 4; j++){
+				if(i == j){
+					if(combination[i].color == combinationArray[j].color){
+						goodColorGoodPlace+=1;
+					}
+				
+						
+				}
+				else{
+					if(combination[i].color == combinationArray[j].color){
+						goodColorBadPlace+=1;
+					}
+				}
+			}
+		}
+		responseArray = {goodColorGoodPlace , goodColorBadPlace};
+	}
+
 }
