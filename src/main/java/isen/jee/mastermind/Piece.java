eb3aec5ab8dc93bf13b0745b5a8775795f6efd6c
damederes <jean.delbecque@isen-lille.fr>
2016-01-12 09:44:19 +0100

    ajout code piece

--- src/main/java/isen/jee/mastermind/Piece.java
+++ src/main/java/isen/jee/mastermind/Piece.java
@@ -1,5 +1,17 @@
 package isen.jee.mastermind;
 
+import java.util.Random;
+
 public class Piece {
 
+	public enum Colors {
+		RED, YELLOW, GREEN, BLUE, ORANGE, WHITE, PURPLE, BLACK
+	}
+	
+	public Colors color;
+
+	public Colors chooseRandomColor(){
+		int pick = new Random().nextInt(Colors.values().length);
+		return Colors.values()[pick];
+	}
 }
