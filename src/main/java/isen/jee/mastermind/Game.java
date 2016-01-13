4021a0272bc08ae0217c26bb326fc3370f51a360
damederes <jean.delbecque@isen-lille.fr>
2016-01-13 09:27:39 +0100

    commentaires sur le jeu

--- src/main/java/isen/jee/mastermind/Game.java
+++ src/main/java/isen/jee/mastermind/Game.java
@@ -2,4 +2,19 @@ package isen.jee.mastermind;
 
 public class Game {
 
+	// l'utilisateur entre une combinaison
+	
+	// la combinaison est comparée à celle créée aléatoirement
+	
+	// le résultat est affiché sur le côté
+	
+	// l'utilisateur peut entrer une nouvelle combinaison
+	
+	// si les deux combinaisons sont identiques : WINNER
+	
+	// si les 10 essais sont faits : LOSER
+	
+	
+	
+	
 }
